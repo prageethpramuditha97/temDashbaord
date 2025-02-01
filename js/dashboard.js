2d90fe1796ee28519dee3079806754bd0d8a8228
eclubit <eclubit2024@gmail.com>
2025-02-01 10:19:24 +0530

    added

--- js/dashboard.js
+++ js/dashboard.js
@@ -1,10 +1,19 @@
-
+var filter = "srno";
 
 var server = "https://officemanagement-01725a3093a3.herokuapp.com/";
 function getDetails(){
 	var url = server + "api/student_info/"
+	var sr = "";
+	if(filter == "srno"){
+		sr = document.getElementById("SRNo").value;
+	}
+	if(filter == "email"){
+		sr = document.getElementById("email").value;
+	}
+	
 	var obj = {
-		"SR_no" : document.getElementById("SRNo").value
+		"SR_no" : sr,
+		"type" : filter,
 	}
 	document.getElementById("loading").classList.remove("d-none");
 	
@@ -12,8 +21,8 @@ function getDetails(){
 	document.getElementById("name").innerHTML = "";
 	document.getElementById("Memebr_ID").innerHTML = "";
 	document.getElementById("nic").innerHTML = "";
-	document.getElementById("email").innerHTML = "";
-	document.getElementById("mobile").innerHTML = "";
+	document.getElementById("email_on_list").innerHTML = "";
+	document.getElementById("mobile_on_list").innerHTML = "";
 	document.getElementById("activation").innerHTML = "";
 	document.getElementById("marketplace").innerHTML = "";
 	document.getElementById("courseType").innerHTML = "";
@@ -34,26 +43,33 @@ function getDetails(){
 		url: url,
 		data : obj,
 		success: function(resultData) {
-			var fullName = resultData.firstname.charAt(0).toUpperCase() + resultData.firstname.slice(1) + " " + resultData.lastname.charAt(0).toUpperCase() + resultData.lastname.slice(1);
-			document.getElementById("name").innerHTML = fullName;
-			document.getElementById("Memebr_ID").innerHTML = resultData.username;
-			document.getElementById("nic").innerHTML = resultData.nic;
-			document.getElementById("email").innerHTML = resultData.email;
-			document.getElementById("mobile").innerHTML = resultData.mobile;
-			document.getElementById("activation").innerHTML = resultData.subscription;
-			document.getElementById("marketplace").innerHTML = resultData.marketplace;
-			document.getElementById("courseType").innerHTML = resultData.class_type;
-			document.getElementById("Medium").innerHTML = resultData.medium;
-			
-			document.getElementById("j").innerHTML = resultData.join_date;
-			document.getElementById("a").innerHTML = resultData.activation_date ;
-			document.getElementById("h").innerHTML = resultData.half_act_date;
-			document.getElementById("f").innerHTML = resultData.full_act_date;
-			
-			document.getElementById("Sponser").innerHTML = "CCO" + resultData.sponsor;
-			document.getElementById("Sponser_Name").innerHTML = resultData.sponsor_name;
-			document.getElementById("loading").classList.add("d-none");
 			console.log(resultData);
+			if(resultData != 'No user details were found.'){
+				var fullName = resultData.firstname.charAt(0).toUpperCase() + resultData.firstname.slice(1) + " " + resultData.lastname.charAt(0).toUpperCase() + resultData.lastname.slice(1);
+				document.getElementById("name").innerHTML = fullName;
+				document.getElementById("Memebr_ID").innerHTML = resultData.username;
+				document.getElementById("nic").innerHTML = resultData.nic;
+				document.getElementById("email_on_list").innerHTML = resultData.email;
+				document.getElementById("mobile_on_list").innerHTML = resultData.mobile;
+				document.getElementById("activation").innerHTML = resultData.subscription;
+				document.getElementById("marketplace").innerHTML = resultData.marketplace;
+				document.getElementById("courseType").innerHTML = resultData.class_type;
+				document.getElementById("Medium").innerHTML = resultData.medium;
+				
+				document.getElementById("j").innerHTML = resultData.join_date;
+				document.getElementById("a").innerHTML = resultData.activation_date ;
+				document.getElementById("h").innerHTML = resultData.half_act_date;
+				document.getElementById("f").innerHTML = resultData.full_act_date;
+				
+				document.getElementById("Sponser").innerHTML = "CCO" + resultData.sponsor;
+				document.getElementById("Sponser_Name").innerHTML = resultData.sponsor_name;
+				document.getElementById("loading").classList.add("d-none");
+				console.log(resultData);
+			}
+			else {
+				document.getElementById("loading").classList.add("d-none");
+				alert("No user found");
+			}
 			
 		},
 		error: function(xhr, status, error) {
@@ -62,11 +78,33 @@ function getDetails(){
 	});
 }
 
+function changeFilter(id){
+	if(id == "srno"){
+		document.getElementById('email').value = "";
+	}
+	if(id == "email"){
+		document.getElementById('SRNo').value = "";
+	}
+	//document.getElementById(filter).classList.remove("btn-success");
+	filter = id;
+	//document.getElementById(filter).classList.add("btn-success");
+	/*if (event.key === "S") {
+		console.log(filterList[filter]);
+		document.getElementById(filterList[filter]).classList.remove("selected_filter");
+		filter = filter + 1;
+		if(filter == 3){
+			filter = 0;
+		}
+		document.getElementById(filterList[filter]).classList.add("selected_filter");
+	}*/
+}
+
 window.addEventListener("keypress", function(event) {
   // If the user presses the "Enter" key on the keyboard
   if (event.key === "Enter") {
     getDetails();
   }
+  
 });
 
 //getDetails(obj);
