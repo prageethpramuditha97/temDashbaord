a700b162e0c0420e2dc2c7c9dc96264bfd6ab31f
eclubit <eclubit2024@gmail.com>
2025-02-12 12:22:21 +0530

    order added

--- js/validation.js
+++ js/validation.js
@@ -18,61 +18,12 @@ function getTimeNow() {
 };
 
 var server = "https://officemanagement-01725a3093a3.herokuapp.com/";
-function validateEmail(){
-	var url = server + "api/student_info/"
-	
-	var obj = {
-		"SR_no" : document.getElementById("email").value,
-		"type" : "email",
-	}
-	document.getElementById("loading").classList.remove("d-none");
-	document.getElementById("msg").innerHTML = "";
-	document.getElementById("msg2").innerHTML = "";
-	
-	$.ajax({
-		type: 'POST',
-		url: url,
-		data : obj,
-		success: function(resultData) {
-			if(resultData != 'No user details were found.' && resultData.username.replace(/[0-9]/g, '') == "CCO"){
-				localStorage.setItem("login_obj_for_validate_cco4", JSON.stringify(resultData));
-				localStorage.setItem("login_obj_for_validate_cco4_time", getTimeNow());
-				sendOTP();
-				document.getElementById("loading").classList.add("d-none");
-				document.getElementById("msg2").innerHTML = resultData.username;
-			}
-			else {
-				document.getElementById("loading").classList.add("d-none");
-				document.getElementById("msg").innerHTML = "Invalid Email Address";
-			}
-			
-		},
-		error: function(xhr, status, error) {
-			console.log(error);
-		}
-	});
-}
 
-function sendOTP(){
-	var url = server + "api/cco4/send_otp/"
-	$.ajax({
-		type: 'POST',
-		url: url,
-		data : JSON.parse(localStorage.getItem("login_obj_for_validate_cco4")),
-		success: function(resultData) {
-			
-			
-		},
-		error: function(xhr, status, error) {
-			console.log(error);
-		}
-	});
-}
 
 window.addEventListener("keypress", function(event) {
   // If the user presses the "Enter" key on the keyboard
   if (event.key === "Enter") {
-	validateEmail();
+	verifyOTP()
   }
   
 });
